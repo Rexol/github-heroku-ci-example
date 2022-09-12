e52e1fd776a9a917c0e8e1c20d3bd00690dcdd7d
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2022-09-12 15:29:45 +0300

    message changed1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('UPD: Hello World!');
+  res.send('UPD1: Hello World!');
 })
 
 app.listen(port, () => {
